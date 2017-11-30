d2e412a0939e52d051489b00978cfd818877fab0
cubemaster21 <cubemaster21@comcast.net>
2017-11-30 13:39:29 -0500

    Added start of basic menus
    -Added scene for main menu
    -Added scene for mistake selection
    -Started scene transition code

---
+++ Assets/UI/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void tutorialButtonPressed()
+    {
+
+    }
+    public void commonMistakesButtonPressed()
+    {
+        Debug.Log("clicked");
+        SceneManager.LoadScene("UI/CommonMistakeSelector", LoadSceneMode.Single);
+    }
+
+}
